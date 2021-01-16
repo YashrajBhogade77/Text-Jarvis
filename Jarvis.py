967eb8a539b53502387e71226a3ec5b5b963c760
Yashraj <77477961+YashrajBhogade77@users.noreply.github.com>
2021-01-16 15:40:44 +0530

    Jarvis.py
    This is a simple jarvis  which displays only text you can add pyttsx3 to  make a audible Jarvis.
    You can feed him all your daily works and he'll remember that. Thank you

---
+++ Jarvis.py
@@ -0,0 +1,11 @@
+print("welcome to my text Jarvis")
+Intake = input ("Enter your query here : ")
+def scan() :
+if "google" in Intake :
+webbrowser.open("https://google.com")
+else:
+print ("ok")
+#You can add more elif as per your construction
+#you can also so put names in the place of Google
+#thanks for downloading my code
+#please follow me
